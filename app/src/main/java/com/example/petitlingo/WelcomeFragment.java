742e7cd3536e17be552de5a722a7afd9c411d0da
Bypus <joris.gre@hotmail.fr>
2023-11-20 14:04:51 +0100

    Fichiers welcome

---
+++ app/src/main/java/com/example/petitlingo/WelcomeFragment.java
@@ -0,0 +1,73 @@
+package com.example.petitlingo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.petitlingo.databinding.FragmentWelcomeBinding;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link WelcomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class WelcomeFragment extends Fragment {
+
+    private FragmentWelcomeBinding binding;
+
+    public WelcomeFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment WelcomeFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static WelcomeFragment newInstance() {
+        WelcomeFragment fragment = new WelcomeFragment();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.playButton.setEnabled(true);
+
+//        binding.playButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                FragmentManager fragmentManager = getParentFragmentManager();
+//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//                CalculFragment calculFragment = CalculFragment.newInstance();
+//                fragmentTransaction.add(R.id.fragmentContainerView, calculFragment);
+//                fragmentTransaction.commit();
+//            }
+//        });
+    }
+}
